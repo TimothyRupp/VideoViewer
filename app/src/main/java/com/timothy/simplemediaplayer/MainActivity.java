7b2fb1dd63d384300ec274e5c68565bc3fda0274
Timothy Rupp <tim.rupp@gmail.com>
2016-07-21 09:34:06 -0400

    Add videoView

--- app/src/main/java/com/timothy/simplemediaplayer/MainActivity.java
+++ app/src/main/java/com/timothy/simplemediaplayer/MainActivity.java
@@ -1,13 +1,28 @@
 package com.timothy.simplemediaplayer;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String URL = "http://techslides.com/demos/sample-videos/small.mp4";
+
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Uri uri = Uri.parse(URL);
+
+        videoView = (VideoView) findViewById(R.id.videoView);
+
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
     }
 }
